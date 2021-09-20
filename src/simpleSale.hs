fb0c28f4cc463f9f71644544a580d1f003d4aa07
Ülla <90785712+artistulla@users.noreply.github.com>
2021-09-20 19:11:01 +0000

    Update simpleSale.hs
    Update to use traceIfFalse condition, seperaated by $ in the line

--- src/simpleSale.hs
+++ src/simpleSale.hs
@@ -29,7 +29,9 @@ import            Text.Printf         (printf)
 -- Create the validator, as inlinable
 {-# INLINABLE saleValidator #-}
 saleValidator :: () -> Integer -> ScriptContext -> Bool
-saleValidator _ r _ = r == 27
+saleValidator _ r _ = traceIfFalse "Incorrect Redeemer!" $ r == 27
+-- Following would return no "nice" error, new above using traceIfFalse
+-- saleValidator _ r _ = r == 27
   -- Redeemer must match the integer 27 or fails .. new code above ^
   -- | r == I 27 = ()
   -- | otherwise = traceError "Incorrect Redeemer!"
